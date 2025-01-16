d7539d6378ec7ce6363e617e764e12f64a6eddb3
sharadindu das <sharadindudas774@gmail.com>
2025-01-16 21:05:18 +0530

    feat: Closures in javascript

---
+++ 08_closures.js
@@ -0,0 +1,24 @@
+//! Closures 1
+// function x() {
+//   var a = 10;
+//   function y() {
+//     console.log(a);
+//   }
+//   return y;
+// }
+// var z = x();
+// z();
+
+//! Closures 2
+// function z() {
+//   var b = 900;
+//   function x() {
+//     var a = 10;
+//     function y() {
+//       console.log(a, b);
+//     }
+//     y();
+//   }
+//   x();
+// }
+// z();
